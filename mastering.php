776145e4ef4748469139c891db8efe894ff3a587
Natan Rocha Batista <natan.rocha.batista@gmail.com>
2018-06-18 21:11:34 -0300

    The script gets the current date and insert it into the database

--- mastering.php
+++ mastering.php
@@ -6,6 +6,8 @@ $startTime = new DateTime();
 echo "Started at: ".$startTime->format('Y-m-d H:i:s')."\n";
 echo "Counting work time...\n";
 
+
+
 do {
 	$finish = readline("Type 's' to stop the counter:");
 } while ($finish != 's');
@@ -14,9 +16,9 @@ $endTime = new DateTime();
 
 $diff = $endTime->diff($startTime, true);
 
-// echo "Hours: ".$diff->h."\n";
-// echo "Minutes: ".$diff->i."\n";
-// echo "Seconds: ".$diff->s."\n";
+echo "Hours: ".$diff->h."\n";
+echo "Minutes: ".$diff->i."\n";
+echo "Seconds: ".$diff->s."\n";
 
 $hoursToSec = $diff->h * 3600;
 $minToSec = $diff->i * 60;
@@ -24,18 +26,24 @@ $sec = $diff->s;
 
 $totalSec = $hoursToSec+$minToSec+$sec;
 
+$now = time();
+$today = date("Y-m-d", $now);
 
 
-insertField($totalSec);
+// $formattedDate = $startTime->y."-".$startTime->m."-".$startTime->d;
+// echo "Data formatada: ".$formattedDate;
 
-Connection::insert('web_development', '2018-06-18', $totalSec);
 
-function insertField($totalSec) {
+insertField($totalSec, $today);
+
+Connection::insert('web_development', $today, $totalSec);
+
+function insertField($totalSec, $today) {
 	$field = null;
 	while ($field == null) {
 
 		echo "Choose the field:\n";
-		$option = readline("1 - Coding 	2 - UX    3 - SEO    4 - Engineering\n");
+		$option = readline("1 - Coding    2 - UX    3 - SEO    4 - Engineering\n");
 
 		switch ($option) {
 			case 1:
@@ -55,16 +63,16 @@ function insertField($totalSec) {
 				echo "Please, choose the right field\n";
 		}
 	}
-	Connection::insert($field, '2018-06-18', $totalSec);
+	Connection::insert($field, $today, $totalSec);
 	echo "Chosen field: ".$field."\n";
 	if ($field == 'coding') {
-		insertLanguage($totalSec);
+		insertLanguage($totalSec, $today);
 	}
 
 	return $field;
 }
 
-function insertLanguage($totalSec) {
+function insertLanguage($totalSec, $today) {
 	$language = null;
 	while ($language == null) {
 
@@ -83,7 +91,7 @@ function insertLanguage($totalSec) {
 				echo "Please, choose the right language\n";
 		}
 	}
-	Connection::insert($language, '2018-06-18', $totalSec);
+	Connection::insert($language, $today, $totalSec);
 	echo "Chosen language: ".$language."\n";
 }
 
